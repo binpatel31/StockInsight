06ba92ba508ef20c31516fadde1a2f5cf9d7c059
binpatel31 <binpatel31@gmail.com>
2020-09-25 17:31:14 -0400

    removed Data folder

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 setup(
   name='foo'
   version='1.0'
-  description='SE Homework'
-  author='Devarsh'
-  author_email='dshah3@ncsu.edu'
+  description='SE Project'
+  author='Bin Patel'
+  author_email='bpatel24@ncsu.edu'
   packages=setuptools.find_packages(),
   python_requires='>=3.6',
 )
